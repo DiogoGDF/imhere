6d3b629c2c90b5e4e07adea1c6e4c08f3b963653
Diogo Garbinato <diogogarbinato@gmail.com>
2024-09-25 10:25:58 -0300

    feat: styleSheet

--- App.tsx
+++ App.tsx
@@ -1,23 +1,32 @@
-import {  Text, View } from 'react-native';
+import {  StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{
-        flex: 1,
-        backgroundColor: '#131016',
-        padding: 30
-    }}>
-        <Text style={{
-            color: 'white',
-            fontSize: 24,
-            fontWeight: 'bold',
-            marginTop: 48
-        }}>
+    <View style={styles.container}>
+        <Text style={styles.eventName}>
             Hello, World!
         </Text>
-        <Text style={{color: '#6B6B6B'}}>
+        <Text style={styles.eventDate}>
             Quarta, 25 de Setembro de 2024
         </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#131016',
+        padding: 30
+    },
+    eventName: {
+        color: 'white',
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 48
+    },
+    eventDate: {
+        color: '#6B6B6B',
+        fontSize: 16
+    }
+})
